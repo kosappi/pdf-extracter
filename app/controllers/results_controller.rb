41352d799788779385b6e9bb76fa81cd24c35dfc
kosappi <kosaka.ryuichi@misoca.jp>
2018-10-07 20:27:23 +0900

    Add routes for new and create

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,4 +1,10 @@
 class ResultsController < ApplicationController
   def index
   end
+
+  def new
+  end
+
+  def create
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'results#index'
-  get 'results/index', to: 'results#index'
+  get 'index', to: 'results#index'
+  get 'new', to: 'results#new'
+  post 'create', to: 'results#create'
 end
